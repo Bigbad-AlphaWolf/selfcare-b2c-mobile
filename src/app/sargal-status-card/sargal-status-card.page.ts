84418ee95c35c7ac4f050c586633f7e2e8e8daec
ext_kebe04 <tyny95@outlook.fr>
2019-11-20 16:19:46 +0000

    implementation sargal status

--- src/app/sargal-status-card/sargal-status-card.page.ts
+++ src/app/sargal-status-card/sargal-status-card.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { SargalService } from "../services/sargal-service/sargal.service";
+import { SargalStatusModel } from "src/shared";
+import { Router } from "@angular/router";
+import { DashboardService } from "../services/dashboard-service/dashboard.service";
+import * as SecureLS from "secure-ls";
+const ls = new SecureLS({ encodingType: "aes" });
 
 @Component({
   selector: "app-sargal-status-card",
@@ -6,9 +12,49 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./sargal-status-card.page.scss"]
 })
 export class SargalStatusCardPage implements OnInit {
-  sargalStatus: string = "gold";
+  sargalStatus: string;
+  loadingStatus: boolean;
+  hasError: boolean;
+  title = "Ma carte ";
+  expiredStatus: boolean;
+  currentNumber: string;
+  name: string;
 
-  constructor() {}
+  constructor(
+    private sargalService: SargalService,
+    private router: Router,
+    private dashboardService: DashboardService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCustomerSargalStatus();
+    this.currentNumber = this.dashboardService.getCurrentPhoneNumber();
+    const user = ls.get("user");
+    this.name = user.firstName + " " + user.lastName;
+  }
+
+  getCustomerSargalStatus() {
+    this.loadingStatus = true;
+    this.hasError = false;
+    this.expiredStatus = false;
+    this.sargalService.getCustomerSargalStatus().subscribe(
+      (sargalStatus: SargalStatusModel) => {
+        if (sargalStatus.valid) {
+          this.sargalStatus = sargalStatus.status;
+          this.title += sargalStatus.status;
+        } else {
+          this.expiredStatus = true;
+        }
+        this.loadingStatus = false;
+      },
+      (err: any) => {
+        this.loadingStatus = false;
+        if (err.status && err.status === 400) {
+          this.router.navigate(["/dashboard"]);
+        } else {
+          this.hasError = true;
+        }
+      }
+    );
+  }
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,10 +3,10 @@ import {
   UserConsommations,
   UserConsommation,
   ItemUserConso
-} from 'src/app/dashboard';
-import * as SecureLS from 'secure-ls';
-import { HTTP } from '@ionic-native/http/ngx';
-const ls = new SecureLS({ encodingType: 'aes' });
+} from "src/app/dashboard";
+import * as SecureLS from "secure-ls";
+import { HTTP } from "@ionic-native/http/ngx";
+const ls = new SecureLS({ encodingType: "aes" });
 export const REGEX_NUMBER: RegExp = /^((\+221|00221|221) ?)?(7[7-8]{1}) ?([0-9]{3}) ?([0-9]{2}) ?([0-9]{2})$/;
 export const REGEX_NUMBER_OM: RegExp = /^((\+221|00221|221) ?)?(7(0|6|7|8){1}) ?([0-9]{3}) ?([0-9]{2}) ?([0-9]{2})$/;
 export const REGEX_FIX_NUMBER: RegExp = /^((\+221|00221|221) ?)?(33) ?([0-9]{3}) ?([0-9]{2}) ?([0-9]{2})$/;
@@ -16,54 +16,54 @@ export const REGEX_NAME = /^([^0-9_!¡?÷?¿/+=,.@#$%ˆ&*(){}|~<>;:\]\[-]){1,}$/
 export const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const REGEX_DIGIT = /\d/;
 
-export const USER_CONS_CATEGORY_CALL = 'APPEL';
-export const USER_CONS_CATEGORY_INTERNET = 'INTERNET';
-export const USER_CONS_CATEGORY_SMS = 'SMS';
+export const USER_CONS_CATEGORY_CALL = "APPEL";
+export const USER_CONS_CATEGORY_INTERNET = "INTERNET";
+export const USER_CONS_CATEGORY_SMS = "SMS";
 export const USER_CALL_SUMMARY_CONSO_CODES = [1, 6, 9];
-export const OPERATION_TYPE_PASS_INTERNET = 'PASS_INTERNET';
-export const OPERATION_TYPE_PASS_ILLIMIX = 'PASS_ILLIMIX';
-export const OPERATION_TYPE_SOS = 'SOS';
-export const OPERATION_TYPE_SOS_CREDIT = 'SOS CREDIT';
-export const OPERATION_TYPE_SOS_PASS = 'SOS DATA';
-export const OPERATION_TYPE_SOS_ILLIMIX = 'SOS Illimix';
-export const OPERATION_TYPE_SEDDO_CREDIT = 'SEDDO CREDIT';
-export const OPERATION_TYPE_SEDDO_PASS = 'SEDDO PASS';
-export const OPERATION_TYPE_SEDDO_BONUS = 'SEDDO BONUS';
-export const OPERATION_TYPE_TRANSFER_OM = 'orange-money';
-export const OPERATION_TYPE_RECHARGE_CREDIT = 'RECHARGEMENT_CREDIT';
-export const OPERATION_TYPE_SARGAL_CONVERSION = 'SARGAL_CONVERSION';
-export const OPERATION_TRANSFER_OM = 'TRANSFER_MONEY';
-export const OPERATION_TRANSFER_OM_WITH_CODE = 'TRANSFER_MONEY_WITH_CODE';
+export const OPERATION_TYPE_PASS_INTERNET = "PASS_INTERNET";
+export const OPERATION_TYPE_PASS_ILLIMIX = "PASS_ILLIMIX";
+export const OPERATION_TYPE_SOS = "SOS";
+export const OPERATION_TYPE_SOS_CREDIT = "SOS CREDIT";
+export const OPERATION_TYPE_SOS_PASS = "SOS DATA";
+export const OPERATION_TYPE_SOS_ILLIMIX = "SOS Illimix";
+export const OPERATION_TYPE_SEDDO_CREDIT = "SEDDO CREDIT";
+export const OPERATION_TYPE_SEDDO_PASS = "SEDDO PASS";
+export const OPERATION_TYPE_SEDDO_BONUS = "SEDDO BONUS";
+export const OPERATION_TYPE_TRANSFER_OM = "orange-money";
+export const OPERATION_TYPE_RECHARGE_CREDIT = "RECHARGEMENT_CREDIT";
+export const OPERATION_TYPE_SARGAL_CONVERSION = "SARGAL_CONVERSION";
+export const OPERATION_TRANSFER_OM = "TRANSFER_MONEY";
+export const OPERATION_TRANSFER_OM_WITH_CODE = "TRANSFER_MONEY_WITH_CODE";
 
-export const PAYMENT_MOD_CREDIT = 'CREDIT';
-export const PAYMENT_MOD_OM = 'ORANGE_MONEY';
-export const PAYMENT_MOD_BONUS = 'BONUS';
-export const PAYMENT_MOD_NEXT_RECHARGE = 'NEXT_RECHARGE';
-export const PAYMENT_MOD_SARGAL = 'SARGAL';
-export const CREDIT_APPEL_CATEGORY = 'Crédit Appel';
-export const PAY_WITH_CREDIT = 'Crédit Recharge';
-export const PAY_WITH_OM = 'Orange Money';
-export const PAY_WITH_BONUS = 'Bonus vers Orange';
-export const PAY_ON_NEXT_RECHARGE = 'Prochaine recharge';
-export const PAY_WITH_SARGAL = 'Points Sargal';
+export const PAYMENT_MOD_CREDIT = "CREDIT";
+export const PAYMENT_MOD_OM = "ORANGE_MONEY";
+export const PAYMENT_MOD_BONUS = "BONUS";
+export const PAYMENT_MOD_NEXT_RECHARGE = "NEXT_RECHARGE";
+export const PAYMENT_MOD_SARGAL = "SARGAL";
+export const CREDIT_APPEL_CATEGORY = "Crédit Appel";
+export const PAY_WITH_CREDIT = "Crédit Recharge";
+export const PAY_WITH_OM = "Orange Money";
+export const PAY_WITH_BONUS = "Bonus vers Orange";
+export const PAY_ON_NEXT_RECHARGE = "Prochaine recharge";
+export const PAY_WITH_SARGAL = "Points Sargal";
 
-export const KIRENE_Formule = 'New Kirene Avec Orange';
-export const CODE_KIRENE_Formule = '9134';
-export const JAMONO_NEW_SCOOL_CODE_FORMULE = '9131';
-export const JAMONO_ALLO_CODE_FORMULE = '9132';
-export const JAMONO_MAX_CODE_FORMULE = '9133';
+export const KIRENE_Formule = "New Kirene Avec Orange";
+export const CODE_KIRENE_Formule = "9134";
+export const JAMONO_NEW_SCOOL_CODE_FORMULE = "9131";
+export const JAMONO_ALLO_CODE_FORMULE = "9132";
+export const JAMONO_MAX_CODE_FORMULE = "9133";
 
-export const MAIL_URL = 'mailto:serviceclient@orange-sonatel.com';
-export const NO_AVATAR_ICON_URL = '/assets/images/profil-mob.png';
-export const ASSISTANCE_URL = 'https://assistance.orange.sn';
-export const FACEBOOK_URL = 'https://m.me/165622776799685';
+export const MAIL_URL = "mailto:serviceclient@orange-sonatel.com";
+export const NO_AVATAR_ICON_URL = "/assets/images/profil-mob.png";
+export const ASSISTANCE_URL = "https://assistance.orange.sn";
+export const FACEBOOK_URL = "https://m.me/165622776799685";
 export const TWITTER_URL =
-  'https://twitter.com/messages/compose?recipient_id=733327435299729408';
+  "https://twitter.com/messages/compose?recipient_id=733327435299729408";
 
-export const VALID_IMG_EXTENSIONS = ['jpg', 'jpeg', 'png'];
+export const VALID_IMG_EXTENSIONS = ["jpg", "jpeg", "png"];
 
-export const CREDIT = 'crédit';
-export const BONUS = 'bonus';
+export const CREDIT = "crédit";
+export const BONUS = "bonus";
 
 // Maximum size of avatar image allowed in bytes : 3 Mo ou 1024Ko
 export const MAX_USER_AVATAR_UPLOAD_SIZE = 3072;
@@ -123,10 +123,10 @@ export function formatDataVolume(volumeInKiloOctet) {
       result.push(`${rest} Ko`);
     }
   }
-  return result.join(' ');
+  return result.join(" ");
 }
-export function formatDate(dateString: string, seperator = '/') {
-  const [year, month, day] = dateString.split('-');
+export function formatDate(dateString: string, seperator = "/") {
+  const [year, month, day] = dateString.split("-");
   return `${day}${seperator}${month}${seperator}${year}`;
 }
 
@@ -134,42 +134,42 @@ export function userFriendlyTime(seconds: number): string {
   let hh: number;
   let mn: number;
   let ss: number;
-  let time = '';
+  let time = "";
   hh = Math.floor(seconds / 3600);
   seconds = seconds % 3600;
   if (hh !== 0) {
-    time = time + '' + hh + ' heures ';
+    time = time + "" + hh + " heures ";
   }
 
   mn = Math.floor(seconds / 60);
   seconds = seconds % 60;
   if (mn !== 0) {
-    time = time + '' + mn + ' minutes ';
+    time = time + "" + mn + " minutes ";
   }
 
   if (seconds < 60 && seconds !== 0) {
     if (!mn && hh > 0) {
-      time = time + '' + 0 + ' minutes ';
+      time = time + "" + 0 + " minutes ";
     }
     ss = seconds;
-    time = time + '' + ss + ' secondes';
+    time = time + "" + ss + " secondes";
   }
   return time;
 }
 
 export const months = [
-  'Janvier',
-  'Février',
-  'Mars',
-  'Avril',
-  'Mai',
-  'Juin',
-  'Juillet',
-  'Août',
-  'Septembre',
-  'Octobre',
-  'Novembre',
-  'Décembre'
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre"
 ];
 
 export interface PassInfoModel {
@@ -308,7 +308,8 @@ export function getListPassFilteredByLabelAndPaymentMod(
   selectedLabel: string,
   listPass: (
     | (PassInfoModel | PromoPassModel)
-    | (PassIllimModel | PromoPassIllimModel))[],
+    | (PassIllimModel | PromoPassIllimModel)
+  )[],
   paymentMod: string
 ) {
   let listPassFiltered = [];
@@ -392,24 +393,24 @@ export function computeConsoHistory(consos) {
 export function generateUUID() {
   let d = new Date().getTime();
   if (
-    typeof performance !== 'undefined' &&
-    typeof performance.now === 'function'
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function"
   ) {
     d += performance.now(); // use high-precision timer if available
   }
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
     // tslint:disable-next-line: no-bitwise
     const r = (d + Math.random() * 16) % 16 | 0;
     d = Math.floor(d / 16);
     // tslint:disable-next-line: no-bitwise
-    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
   });
 }
 
 export const listLibelleCodeOperationOM = [
-  { operationCode: 'operation-100', operationLibelle: 'deplafonnement' },
-  { operationCode: 'operation-200', operationLibelle: 'creation-compte' },
-  { operationCode: 'operation-300', operationLibelle: 'reclamation' }
+  { operationCode: "operation-100", operationLibelle: "deplafonnement" },
+  { operationCode: "operation-200", operationLibelle: "creation-compte" },
+  { operationCode: "operation-300", operationLibelle: "reclamation" }
 ];
 
 export function getOperationCodeActionOM(libelle: string) {
@@ -423,11 +424,11 @@ export function getOperationCodeActionOM(libelle: string) {
 }
 
 export const FILENAME_OPEN_OM_ACCOUNT =
-  'formulaire_inscription_om_original.pdf';
+  "formulaire_inscription_om_original.pdf";
 export const FILENAME_DEPLAFONNEMENT_OM_ACCOUNT =
-  'formulaire_deplafonnement_original.pdf';
+  "formulaire_deplafonnement_original.pdf";
 export const FILENAME_ERROR_TRANSACTION_OM =
-  'formulaire_erreur_transaction_original.pdf';
+  "formulaire_erreur_transaction_original.pdf";
 
 export interface GiftSargalCategoryItem {
   id: number;
@@ -460,11 +461,11 @@ export interface FormuleMobileModel {
   profil: ProfilModel;
   sos: any;
   tarifFormules: TarifFormuleModel[];
-  type: 'MOBILE' | 'FIXE';
+  type: "MOBILE" | "FIXE";
 }
 
 export interface TarifFormuleModel {
-  categorie: 'APPEL' | 'SMS';
+  categorie: "APPEL" | "SMS";
   code: number;
   description: string;
   id: number;
@@ -480,13 +481,13 @@ export interface ProfilModel {
 
 export function getLastUpdatedDateTimeText() {
   const date = new Date();
-  const lastDate = `${('0' + date.getDate()).slice(-2)}/${(
-    '0' +
+  const lastDate = `${("0" + date.getDate()).slice(-2)}/${(
+    "0" +
     (date.getMonth() + 1)
   ).slice(-2)}/${date.getFullYear()}`;
   const lastDateTime =
     `${date.getHours()}h` +
-    (date.getMinutes() < 10 ? '0' : '') +
+    (date.getMinutes() < 10 ? "0" : "") +
     date.getMinutes();
   return `${lastDate} à ${lastDateTime}`;
 }
@@ -500,11 +501,11 @@ export interface OmAuthInfo {
 
 export function getHeaders(nHttp: HTTP) {
   const headers: any = {};
-  nHttp.setDataSerializer('json');
-  headers['Content-Type'] = 'application/json';
-  const token = ls.get('token');
+  nHttp.setDataSerializer("json");
+  headers["Content-Type"] = "application/json";
+  const token = ls.get("token");
   if (token) {
-    headers.Authorization = 'Bearer ' + token;
+    headers.Authorization = "Bearer " + token;
   }
   return headers;
 }
@@ -536,7 +537,7 @@ export interface ItemBesoinAide {
 
 // this method removes prefix 221 +221 00221
 export function formatPhoneNumber(phoneNumber: string) {
-  const phoneNumberWithoutSpaces = phoneNumber.replace(/\s/g, '');
+  const phoneNumberWithoutSpaces = phoneNumber.replace(/\s/g, "");
   const res = phoneNumberWithoutSpaces.slice(
     phoneNumberWithoutSpaces.length - 9,
     phoneNumberWithoutSpaces.length
@@ -558,7 +559,12 @@ export interface WelcomeStatusModel {
   status: string;
   type: string; // 'RECHARGE' or 'PASS'
   value: {
-      amount: number;
-      unit: string;
+    amount: number;
+    unit: string;
   };
+}
+
+export interface SargalStatusModel {
+  valid: boolean;
+  status: string;
 }
