3ea2eb4fe1e356987dae221de3fbddb1c7f4599f
ext_kebe04 <tyny95@outlook.fr>
2020-02-18 16:55:16 +0000

    remove unused components

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,14 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { DashboardPage } from './dashboard.page';
-import { DashboardPrepaidHybridComponent } from './dashboard-prepaid-hybrid/dashboard-prepaid-hybrid.component';
-import { DashboardPostpaidComponent } from './dashboard-postpaid/dashboard-postpaid.component';
-import { DashboardKireneComponent } from './dashboard-kirene/dashboard-kirene.component';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { SharedModule } from 'src/shared/shared.module';
-import { DashboardHomePrepaidComponent } from './dashboard-home-prepaid/dashboard-home-prepaid.component';
-import { DashboardPostpaidFixeComponent } from './dashboard-postpaid-fixe/dashboard-postpaid-fixe.component';
-
 const routes: Routes = [
   {
     path: '',
@@ -28,13 +22,6 @@ const routes: Routes = [
     SharedModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [
-    DashboardPage,
-    DashboardPrepaidHybridComponent,
-    DashboardPostpaidComponent,
-    DashboardPostpaidFixeComponent,
-    DashboardKireneComponent,
-    DashboardHomePrepaidComponent
-  ]
+  declarations: [DashboardPage]
 })
 export class DashboardPageModule {}
