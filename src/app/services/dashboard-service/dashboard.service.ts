84cfc0014d9e67d972bdd5bec50c8ab5dc672b7b
bigbadwolf <mohamedabdoulkarim.diop2@orange-sonatel.com>
2020-03-13 01:10:33 +0000

    fix conso postpaid

--- src/app/services/dashboard-service/dashboard.service.ts
+++ src/app/services/dashboard-service/dashboard.service.ts
@@ -151,7 +151,7 @@ export class DashboardService {
     return this.http.get(`${postpaidUserConsoEndpoint}/${this.msisdn}`).pipe(
       map(
         (res: any) => {
-          return this.processConso(res);
+          return this.processConso(res,true);
         },
         error => {
           const lastLoadedConso = ls.get(`lastConso_${this.msisdn}`);
@@ -293,8 +293,8 @@ export class DashboardService {
       );
   }
 
-  processConso(conso: any) {
-    if (conso && conso.length) {
+  processConso(conso: any, consoPostpaid?:boolean) {
+    if (conso && conso.length || consoPostpaid) {
       const lastUpdateConsoDate = this.getCurrentDate();
       ls.set(`lastConso_${this.msisdn}`, conso);
       ls.set(`lastUpdateConsoDate_${this.msisdn}`, lastUpdateConsoDate);
