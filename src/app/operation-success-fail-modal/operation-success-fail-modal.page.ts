e496690d363741f1b8816c009cb2eedbb5a2de0e
ext_kebe04 <tyny95@outlook.fr>
2020-04-15 09:50:56 +0000

    success page buy pass

---
+++ src/app/operation-success-fail-modal/operation-success-fail-modal.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DashboardService } from '../services/dashboard-service/dashboard.service';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-operation-success-fail-modal',
+  templateUrl: './operation-success-fail-modal.page.html',
+  styleUrls: ['./operation-success-fail-modal.page.scss'],
+})
+export class OperationSuccessFailModalPage implements OnInit {
+  @Input() passBought: any;
+  @Input() success: boolean;
+  @Input() recipientMsisdn: string;
+  @Input() recipientName: string;
+  @Input() buyForMe: boolean;
+  @Input() paymentMod: string;
+  @Input() msisdnBuyer: string;
+  @Input() errorMsg: string;
+  dateAchat = this.dashboardService.getCurrentDate();
+
+  constructor(
+    private dashboardService: DashboardService,
+    private router: Router,
+    public modalController: ModalController
+  ) {}
+
+  ngOnInit() {}
+
+  terminer() {
+    this.modalController.dismiss();
+    this.router.navigate(['/dashboard']);
+  }
+}
--- src/app/set-payment-channel-modal/set-payment-channel-modal.page.ts
+++ src/app/set-payment-channel-modal/set-payment-channel-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { DashboardService } from '../services/dashboard-service/dashboard.service';
 
 @Component({
   selector: 'set-payment-channel-modal',
@@ -7,12 +8,18 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./set-payment-channel-modal.page.scss'],
 })
 export class SetPaymentChannelModalPage implements OnInit {
-  @Input() price;
+  @Input() pass;
   selectedPaymentChannel: 'CREDIT' | 'ORANGE_MONEY';
+  soldeCredit;
 
-  constructor(public modalController: ModalController) {}
+  constructor(
+    public modalController: ModalController,
+    private dashboardService: DashboardService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getUserConsommations();
+  }
 
   setPaymentChannel(channel: 'CREDIT' | 'ORANGE_MONEY') {
     this.selectedPaymentChannel = channel;
@@ -22,5 +29,17 @@ export class SetPaymentChannelModalPage implements OnInit {
     this.modalController.dismiss({
       paymentMod: this.selectedPaymentChannel,
     });
+  }
+
+  getUserConsommations() {
+    this.dashboardService.getUserConsoInfosByCode().subscribe(
+      (res: any[]) => {
+        const appelConso = res.find((x) => x.categorie === 'APPEL');
+        this.soldeCredit = appelConso.consommations.find(
+          (x) => x.code === 1
+        ).montant;
+      },
+      (err) => {}
+    );
   }
 }
