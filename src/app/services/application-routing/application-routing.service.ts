6ea83deaaca184fbb178e29fd7fde42cef52f1e4
ext_kebe04 <tyny95@outlook.fr>
2020-05-12 16:04:19 +0000

    integration new dashboard

--- src/app/services/application-routing/application-routing.service.ts
+++ src/app/services/application-routing/application-routing.service.ts
@@ -1,69 +1,78 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
-import { OPERATION_TYPE_PASS_INTERNET, OPERATION_TYPE_PASS_ILLIMIX } from 'src/shared';
+import {
+  OPERATION_TYPE_PASS_INTERNET,
+  OPERATION_TYPE_PASS_ILLIMIX,
+} from 'src/shared';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApplicationRoutingService {
+  constructor(private route: Router) {}
 
-  constructor(private route: Router) { }
-
-  goToDashboard(){
-    this.route.navigate(['/dashboard'])
+  goToDashboard() {
+    this.route.navigate(['/dashboard']);
   }
 
-  goToSelectRecepientPassInternet(){
+  goToSelectRecepientPassInternet() {
     let navigationExtras: NavigationExtras = {
       state: {
-        payload: OPERATION_TYPE_PASS_INTERNET
-      }
-    };
-    this.route.navigate(['/select-beneficiary-v2'], navigationExtras)
-  }
-
-  goToListPassInternet(data: any){
-    const payload = Object.assign(data, {purchaseType: OPERATION_TYPE_PASS_INTERNET});
-    let navigationExtras: NavigationExtras = {
-      state: {
-        payload
-      }
-    };
-    this.route.navigate(['/list-pass'], navigationExtras)
-
-  }
-
-  goToListPassIllimix(data: any){
-    const payload = Object.assign(data, {purchaseType: OPERATION_TYPE_PASS_ILLIMIX});
-    let navigationExtras: NavigationExtras = {
-      state: {
-        payload
-      }
-    };
-    this.route.navigate(['/list-pass'], navigationExtras)
-
-  }
-
-  goToSelectRecepientPassIllimix(){
-    let navigationExtras: NavigationExtras = {
-      state: {
-        payload: OPERATION_TYPE_PASS_ILLIMIX
-      }
+        payload: OPERATION_TYPE_PASS_INTERNET,
+      },
     };
     this.route.navigate(['/select-beneficiary-v2'], navigationExtras);
   }
 
-  goToTransfertHubServicesPage(){
-    this.route.navigate(['/transfert-hub-services']);
+  goToListPassInternet(data: any) {
+    const payload = Object.assign(data, {
+      purchaseType: OPERATION_TYPE_PASS_INTERNET,
+    });
+    let navigationExtras: NavigationExtras = {
+      state: {
+        payload,
+      },
+    };
+    this.route.navigate(['/list-pass'], navigationExtras);
   }
 
-  goToTransfertMoneyPage(){
-    this.route.navigate(['/transfert-om-hub-services'])
+  goToListPassIllimix(data: any) {
+    const payload = Object.assign(data, {
+      purchaseType: OPERATION_TYPE_PASS_ILLIMIX,
+    });
+    let navigationExtras: NavigationExtras = {
+      state: {
+        payload,
+      },
+    };
+    this.route.navigate(['/list-pass'], navigationExtras);
   }
-  goToTransfertCreditPage(){
+
+  goToSelectRecepientPassIllimix() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        payload: OPERATION_TYPE_PASS_ILLIMIX,
+      },
+    };
+    this.route.navigate(['/select-beneficiary-v2'], navigationExtras);
+  }
+
+  goToTransfertHubServicesPage(purchaseType: 'BUY' | 'TRANSFER') {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        purchaseType,
+      },
+    };
+    this.route.navigate(['/transfert-hub-services'], navigationExtras);
+  }
+
+  goToTransfertMoneyPage() {
+    this.route.navigate(['/transfert-om-hub-services']);
+  }
+  goToTransfertCreditPage() {
     // this.route.navigate(['/'])
   }
-  goToTransfertBonusPage(){
+  goToTransfertBonusPage() {
     // this.route.navigate(['/'])
   }
 }
--- src/app/transfert-hub-services/transfert-hub-services.page.ts
+++ src/app/transfert-hub-services/transfert-hub-services.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationRoutingService } from '../services/application-routing/application-routing.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transfert-hub-services',
@@ -7,45 +8,121 @@ import { ApplicationRoutingService } from '../services/application-routing/appli
   styleUrls: ['./transfert-hub-services.page.scss'],
 })
 export class TransfertHubServicesPage implements OnInit {
-  options: {
+  transferOptions: {
     title: string;
     subtitle: string;
     icon: string;
     type: 'TRANSFERT_MONEY' | 'TRANSFERT_CREDIT' | 'TRANSFERT_BONUS';
     url?: string;
     action?: 'REDIRECT' | 'POPUP';
-}[] = [
-  {
-    title: 'Transfert',
-    subtitle: "d'argent",
-    icon: "/assets/images/icOrangeMoney.png",
-    action: "REDIRECT",
-    type: "TRANSFERT_MONEY",
-    url:""
-  },
-  {
-    title: 'Transfert',
-    subtitle: "de crédit",
-    icon: "/assets/images/ic-top-up-mobile@2x.png",
-    action: "REDIRECT",
-    type: "TRANSFERT_CREDIT",
-    url:""
-  },
-  {
-    title: 'Transfert',
-    subtitle: "de bonus",
-    icon: "/assets/images/ic-reward.png",
-    action: "REDIRECT",
-    type: "TRANSFERT_BONUS",
-    url:""
-  }
-];
-  constructor(private appRouting: ApplicationRoutingService) { }
+  }[] = [
+    {
+      title: 'Transfert',
+      subtitle: "d'argent",
+      icon: '/assets/images/icOrangeMoney.png',
+      action: 'REDIRECT',
+      type: 'TRANSFERT_MONEY',
+      url: '',
+    },
+    {
+      title: 'Transfert',
+      subtitle: 'de crédit',
+      icon: '/assets/images/ic-top-up-mobile@2x.png',
+      action: 'REDIRECT',
+      type: 'TRANSFERT_CREDIT',
+      url: '',
+    },
+    {
+      title: 'Transfert',
+      subtitle: 'de bonus',
+      icon: '/assets/images/ic-reward.png',
+      action: 'REDIRECT',
+      type: 'TRANSFERT_BONUS',
+      url: '',
+    },
+  ];
+  buyOptions: {
+    title: string;
+    subtitle: string;
+    icon: string;
+    type:
+      | 'CREDIT'
+      | 'PASS'
+      | 'PASS_ILLIMIX'
+      | 'PASS_VOYAGE'
+      | 'PASS_INTERNATIONAL';
+    url?: string;
+    action?: 'REDIRECT' | 'POPUP';
+  }[] = [
+    {
+      title: 'Transfert',
+      subtitle: "d'argent",
+      icon: '/assets/images/ic-top-up-mobile@2x.png',
+      action: 'REDIRECT',
+      type: 'CREDIT',
+      url: '',
+    },
+    {
+      title: 'Transfert',
+      subtitle: 'de crédit',
+      icon: '/assets/images/ic-internet-usage@2x.png',
+      action: 'REDIRECT',
+      type: 'PASS',
+      url: '',
+    },
+    {
+      title: 'Transfert',
+      subtitle: 'de bonus',
+      icon: '/assets/images/ic-package-services@2x.png',
+      action: 'REDIRECT',
+      type: 'PASS_ILLIMIX',
+      url: '',
+    },
+    ,
+    {
+      title: 'Transfert',
+      subtitle: 'de bonus',
+      icon: '/assets/images/ic-reward.png',
+      action: 'REDIRECT',
+      type: 'PASS_VOYAGE',
+      url: '',
+    },
+    ,
+    {
+      title: 'Transfert',
+      subtitle: 'de bonus',
+      icon: '/assets/images/ic-reward.png',
+      action: 'REDIRECT',
+      type: 'PASS_INTERNATIONAL',
+      url: '',
+    },
+  ];
+  options = [];
+  constructor(
+    private appRouting: ApplicationRoutingService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      if (
+        this.router.getCurrentNavigation() &&
+        this.router.getCurrentNavigation().extras.state &&
+        this.router.getCurrentNavigation().extras.state.purchaseType
+      ) {
+        const purchaseType = this.router.getCurrentNavigation().extras.state
+          .purchaseType;
+        purchaseType === 'TRANSFER'
+          ? (this.options = this.transferOptions)
+          : (this.options = this.buyOptions);
+      } else {
+        this.appRouting.goToDashboard();
+      }
+    });
   }
 
-  goToDashboard(){
+  goToDashboard() {
     this.appRouting.goToDashboard();
   }
 
@@ -56,25 +133,25 @@ export class TransfertHubServicesPage implements OnInit {
     type: 'TRANSFERT_MONEY' | 'TRANSFERT_CREDIT' | 'TRANSFERT_BONUS';
     url?: string;
     action?: 'REDIRECT' | 'POPUP';
-} ){
+  }) {
     switch (opt.type) {
       case 'TRANSFERT_MONEY':
-        if(opt.action === 'REDIRECT'){
+        if (opt.action === 'REDIRECT') {
           this.appRouting.goToTransfertMoneyPage();
         }
         break;
       case 'TRANSFERT_CREDIT':
-        if(opt.action === 'REDIRECT'){
+        if (opt.action === 'REDIRECT') {
           this.appRouting.goToTransfertCreditPage();
         }
         break;
       case 'TRANSFERT_BONUS':
-        if(opt.action === 'REDIRECT'){
+        if (opt.action === 'REDIRECT') {
           this.appRouting.goToTransfertBonusPage();
         }
         break;
       default:
         break;
     }
-}
+  }
 }
