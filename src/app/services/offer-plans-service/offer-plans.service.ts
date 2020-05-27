46f292361c47bf522832191b5b94c0af262c8491
bigbadwolf <mohamedabdoulkarim.diop2@orange-sonatel.com>
2020-05-27 11:39:40 +0000

    remove mocked data on offer services

--- src/app/services/offer-plans-service/offer-plans.service.ts
+++ src/app/services/offer-plans-service/offer-plans.service.ts
@@ -13,78 +13,10 @@ const getCurrentUserOfferPlansEndpoint = `${SERVER_API_URL}/${CONSO_SERVICE}/api
   providedIn: 'root'
 })
 export class OfferPlansService {
-  mockDataPlans: OfferPlan[] = [
-    {
-        "bpTarget": "Pass internet 75 Mo 250F",
-        "bpValidityDurability": " 100% bonus sous 4h",
-        "typeMPO": "INTERNET",
-        "price": {
-            "currencyCode": "CFA",
-            "taxIncludedAmount": "0.0"
-        },
-        "productOfferingId": "75Mo_100%bonus_14052020",
-        "canBeSubscribed": true,
-        "description": "Bonjour cher client, jusqu a demain , protitez de 100% de bonus sous 4h pour tout achat d'un pass internet 75Mo. #144#4*1*3*4# & #1234#1*1*1*1#",
-        "productOfferingName": "Pass internet 75 Mo 250F= 100% bonus sous 4h"
-    },
-    {
-        "bpTarget": "Pass internet 75 Mo 250F",
-        "bpValidityDurability": " 100% bonus sous 4h",
-        "typeMPO": "ILLIMIX",
-        "price": {
-            "currencyCode": "CFA",
-            "taxIncludedAmount": "0.0"
-        },
-        "productOfferingId": "75Mo_100%bonus_14052020",
-        "canBeSubscribed": true,
-        "description": "Bonjour cher client, jusqu a demain , protitez de 100% de bonus sous 4h pour tout achat d'un pass internet 75Mo. #144#4*1*3*4# & #1234#1*1*1*1#",
-        "productOfferingName": "Pass internet 75 Mo 250F= 100% bonus sous 4h"
-    },
-    {
-        "bpTarget": "Pass internet 75 Mo 250F",
-        "bpValidityDurability": " 100% bonus sous 4h",
-        "typeMPO": "SARGAL",
-        "price": {
-            "currencyCode": "CFA",
-            "taxIncludedAmount": "0.0"
-        },
-        "productOfferingId": "75Mo_100%bonus_14052020",
-        "canBeSubscribed": true,
-        "description": "Bonjour cher client, jusqu a demain , protitez de 100% de bonus sous 4h pour tout achat d'un pass internet 75Mo. #144#4*1*3*4# & #1234#1*1*1*1#",
-        "productOfferingName": "Pass internet 75 Mo 250F= 100% bonus sous 4h"
-    },
-    {
-        "bpTarget": "Pass internet 5 Mo 250F",
-        "bpValidityDurability": " 100% bonus sous 4h",
-        "typeMPO": "INTERNET",
-        "price": {
-            "currencyCode": "CFA",
-            "taxIncludedAmount": "0.0"
-        },
-        "productOfferingId": "75Mo_100%bonus_14052020",
-        "canBeSubscribed": true,
-        "description": "Bonjour cher client, jusqu a demain , protitez de 100% de bonus sous 4h pour tout achat d'un pass internet 75Mo. #144#4*1*3*4# & #1234#1*1*1*1#",
-        "productOfferingName": "Pass internet 75 Mo 250F= 100% bonus sous 4h"
-    },
-    {
-        "bpTarget": "Inscription SARGAL",
-        "bpValidityDurability": "1000F bonus sous 4h",
-        "typeMPO": "AUTRES",
-        "price": {
-            "currencyCode": "CFA",
-            "taxIncludedAmount": "0.0"
-        },
-        "productOfferingId": "InscriptionSARGAL_1000Fbonus_14052020",
-        "canBeSubscribed": true,
-        "description": "Bonjour cher client, jusqu a demain , inscrivez-vous sur SARGAL et gagnez 1000F de bonus sous 4h credit. #221#1#",
-        "productOfferingName": "Inscription SARGAL=1000F bonus sous 4h"
-    }
-];
   constructor(private http: HttpClient, private dashbServ: DashboardService) { }
 
   getCurrentUserOfferPlans(){
     const msisdn = this.dashbServ.getCurrentPhoneNumber();
-    return of(this.mockDataPlans);
-    // return this.http.get(`${getCurrentUserOfferPlansEndpoint}/${msisdn}`);
+     return this.http.get(`${getCurrentUserOfferPlansEndpoint}/${msisdn}`);
   }
 }
