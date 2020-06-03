85894ac040366ca20d2e95823faa173499da716f
Wiyaou BEGBESSOU <ext_wiyaou.begbessou@orange-sonatel.com>
2020-06-03 19:16:28 +0000

    Finalize confirm, succes page

--- src/app/pages/credit-pass-amount/credit-pass-amount.page.ts
+++ src/app/pages/credit-pass-amount/credit-pass-amount.page.ts
@@ -27,8 +27,6 @@ export class CreditPassAmountPage implements OnInit {
     this.title = (getPageTitle(this.opXtras.purchaseType)).title;
   }
 
-
-
   onAmountSelected(amount: string) {
     this.opXtras.amount = amount;
     this.onContinue();
