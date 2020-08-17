3cf9b29fa1259a0ee1508fb575f67d086ad42b11
Wiyaou BEGBESSOU <ext_wiyaou.begbessou@orange-sonatel.com>
2020-08-17 23:40:02 +0000

    Remove bottomsheet blocking config

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { MatBottomSheetContainer, AnimationDurations, AnimationCurves } from '@angular/material';
-import { style, trigger, state, transition, animate } from '@angular/animations';
-MatBottomSheetContainer['decorators'][0].args[0].animations[0] = trigger('state', [
-  state('void, hidden', style({transform: 'translateY(10%)', opacity:.5})),
-  state('visible', style({transform: 'translateY(0%)', opacity:1})),
-  transition('visible => void, visible => hidden',
-      animate(`${AnimationDurations.COMPLEX} ${AnimationCurves.ACCELERATION_CURVE}`)),
-  transition('void => visible',[
-      animate('80ms cubic-bezier(.1, .7, .1, 1)'),
-  ]
-    ),
-]);
+// import { MatBottomSheetContainer, AnimationDurations, AnimationCurves } from '@angular/material';
+// import { style, trigger, state, transition, animate } from '@angular/animations';
+// MatBottomSheetContainer['decorators'][0].args[0].animations[0] = trigger('state', [
+//   state('void, hidden', style({transform: 'translateY(10%)', opacity:.5})),
+//   state('visible', style({transform: 'translateY(0%)', opacity:1})),
+//   transition('visible => void, visible => hidden',
+//       animate(`${AnimationDurations.COMPLEX} ${AnimationCurves.ACCELERATION_CURVE}`)),
+//   transition('void => visible',[
+//       animate('80ms cubic-bezier(.1, .7, .1, 1)'),
+//   ]
+//     ),
+// ]);
 
 if (environment.production) {
   enableProdMode();
