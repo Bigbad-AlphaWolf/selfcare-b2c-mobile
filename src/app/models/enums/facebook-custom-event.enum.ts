70612e03ddec1dc5ce5c188b2c86d109078ff386
Wiyaou BEGBESSOU <ext_wiyaou.begbessou@orange-sonatel.com>
2020-08-20 08:41:37 +0000

    SELFB2C-2466 Intégration du SDK de Facebook

---
+++ src/app/models/enums/facebook-custom-event.enum.ts
@@ -0,0 +1,3 @@
+export enum FacebookCustomEvent {
+    TestEvent = 'TestEvent',
+}
---
+++ src/app/models/enums/facebook-event.enum.ts
@@ -0,0 +1,25 @@
+/*
+** Suivre ce lien pour voir les propriétés requises de chaque évenement
+* https://medium.com/seedlogic/how-to-create-and-use-custom-facebook-conversion-events-using-trackcustom-2f3c5d2ed042
+*/
+export enum FacebookEvent {
+    AddPaymentInfo = 'AddPaymentInfo',
+    AddToCart = 'AddToCart',
+    Purchase = 'Purchase',
+    ViewContent = 'ViewContent',
+    AddToWishlist = 'AddToWishlist',
+    CompleteRegistration = 'CompleteRegistration',
+    Contact = 'Contact',
+    CustomizeProduct = 'CustomizeProduct',
+    Donate = 'Donate',
+    FindLocation = 'FindLocation',
+    InitiateCheckout = 'InitiateCheckout',
+    Lead = 'Lead',
+    PageView = 'PageView',
+    Schedule = 'Schedule',
+    Search = 'Search',
+    StartTrial = 'StartTrial',
+    SubmitApplication = 'SubmitApplication',
+    Subscribe = 'Subscribe',
+
+}
