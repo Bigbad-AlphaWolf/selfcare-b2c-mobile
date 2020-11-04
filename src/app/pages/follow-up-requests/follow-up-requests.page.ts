fc4728c413d164b59de3511a32debd354704cdbb
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2020-11-04 10:19:40 +0000

    SELFB2C-2821 Show different status of demandes and request

--- src/app/pages/follow-up-requests/follow-up-requests.page.ts
+++ src/app/pages/follow-up-requests/follow-up-requests.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DashboardService } from 'src/app/services/dashboard-service/dashboard.service';
 import { tap, takeUntil, delay, catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { RequestOem } from 'src/app/models/request-oem.model';
 import { RequestOemService } from 'src/app/services/request-oem/request-oem.service';
 import { BaseComponent } from 'src/app/base.component';
 import { NavController } from '@ionic/angular';
 import { RequestStatusPage } from '../request-status/request-status.page';
+import { ReclamationType } from 'src/app/models/enums/reclamation.enum';
 
 @Component({
   selector: 'app-follow-up-requests',
@@ -17,10 +18,12 @@ export class FollowUpRequestsPage extends BaseComponent implements OnInit {
   phoneFix: string;
   requests$: Observable<RequestOem[]>;
   isInitRequests: boolean;
+  title = "Suivi Demande ou Réclamation" ;
   @ViewChild('numberInput') numberInput: ElementRef;
   isConfirm: boolean;
   noRequest: boolean;
   isNotValid: boolean;
+  listRequestWithStatus: { "current" : RequestOem, "previous" : RequestOem[], "next": RequestOem[] } = { "current": null, "previous": [], "next": []} ;
   constructor(
     private dashboardService: DashboardService,
     private requestSrvice: RequestOemService,
@@ -38,6 +41,10 @@ export class FollowUpRequestsPage extends BaseComponent implements OnInit {
     this.isNotValid = false;
   }
 
+  initPageTitle(currentRequest: RequestOem) {
+    this.title = currentRequest.type === ReclamationType.REQUEST ? 'Suivi Demande N°'+currentRequest.requestId : 'Suivi Dérangement N°'+currentRequest.requestId;
+  }
+
   initRequests() {
     this.isInitRequests = true;
     this.dashboardService
@@ -52,9 +59,27 @@ export class FollowUpRequestsPage extends BaseComponent implements OnInit {
           this.phoneFix = numbers[0];
           this.requests$ = this.requestSrvice.fetchRequests(this.phoneFix).pipe(
             delay(100),
-            tap((_) => {
+            tap((res: RequestOem[]) => {
               this.isInitRequests = false;
-            })
+              this.listRequestWithStatus.current = res.find((el: RequestOem) => {
+                return el.currentState
+              });
+          
+              this.listRequestWithStatus.previous = res.filter((elt: RequestOem) => {
+                return elt.order < this.listRequestWithStatus.current.order;
+              });
+          
+              this.listRequestWithStatus.next = res.filter((elt: RequestOem) => {
+                return elt.order > this.listRequestWithStatus.current.order;
+              });
+              if(this.listRequestWithStatus.current) {
+                this.initPageTitle(this.listRequestWithStatus.current);
+              }
+            },
+            catchError((err: any) => {
+              this.isInitRequests = false;
+              return of(err)
+            }))
           );
         }),
         takeUntil(this.ngUnsubscribe)
