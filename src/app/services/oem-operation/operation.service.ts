e301dd91e5f53b0d0259ca9887d68892b69ee1a3
ext_kebe04 <tyny95@outlook.fr>
2020-11-18 16:00:31 +0000

    SELFB2C-2848 disabled services not activated in admin

--- src/app/services/oem-operation/operation.service.ts
+++ src/app/services/oem-operation/operation.service.ts
@@ -2,31 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { OffreService } from 'src/app/models/offre-service.model';
 import { DATA_OFFRES_SERVICES } from 'src/app/utils/data';
-import { OFFRE_SERVICES_ENDPOINT } from '../utils/services.endpoints';
-
+import {
+  OFFRE_SERVICES_ENDPOINT,
+  ALL_SERVICES_ENDPOINT,
+} from '../utils/services.endpoints';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OperationService {
-
-  offresServices : any [];
-  
-  constructor(private http: HttpClient) { }
+  offresServices: any[];
+  static AllOffers: OffreService[] = [];
+  constructor(private http: HttpClient) {}
 
   initServicesData(codeFormule?: string) {
     let endpointOffresServices = OFFRE_SERVICES_ENDPOINT;
-    if(codeFormule) {
-      endpointOffresServices += `/${codeFormule}?typeResearch=FORMULE`
+    if (codeFormule) {
+      endpointOffresServices += `/${codeFormule}?typeResearch=FORMULE`;
     }
     return this.http.get(endpointOffresServices).pipe(
-      map((r: any[])=>{
+      map((r: any[]) => {
         this.offresServices = r;
       })
     );
   }
 
-  sortByOrdre(arr:any[]){
-    return arr.sort((r1, r2) =>r1.ordre - r2.ordre);
+  sortByOrdre(arr: any[]) {
+    return arr.sort((r1, r2) => r1.ordre - r2.ordre);
+  }
+
+  getAllServices() {
+    return this.http.get(`${ALL_SERVICES_ENDPOINT}?page=0&size=100`);
   }
 }
