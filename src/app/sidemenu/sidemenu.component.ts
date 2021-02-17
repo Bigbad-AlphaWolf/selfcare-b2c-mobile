349b8e1a6d6fb1678d54c2101e0406e680bf8696
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-02-17 10:58:44 +0000

    set previous url redirection for community

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -155,8 +155,8 @@ export class SidemenuComponent implements OnInit, OnDestroy {
   }
 
   goToAssistancePage() {
-    this.router.navigate(['/community']);
-    return;
+    // this.router.navigate(['/community']);
+    // return;
     this.iab.create(ASSISTANCE_URL, '_self');
     this.followAnalyticsService.registerEventFollow(
       'Sidemenu_Assistance',
