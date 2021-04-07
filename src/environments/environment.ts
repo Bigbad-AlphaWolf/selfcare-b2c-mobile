58f298f2698f706c8188cc9dffccf5db584b18d0
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-04-07 16:29:10 +0000

    SELFB2C-3338 check user has enough rechargement balance to suscribe dalal tones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,8 @@ export const environment = {
   // SERVER_API_URL: 'https://orangeetmoi.orange.sn',
   // SERVER_API_URL: 'https://espaceclientv2.orangebusiness.sn',
   // SERVER_API_URL:  'http://selfcare-b2c-client-http-dsiselfcarebcorangeetmoi-rec.malaw.orange-sonatel.com',
-  SERVER_API_URL:
-    'http://selfcareb2c-client-http-dsiselfcarebc-dev.k8s-test.orange-sonatel.com',
-  // SERVER_API_URL: 'http://10.100.97.175:8712',
+  SERVER_API_URL:  'http://selfcareb2c-client-http-dsiselfcarebc-dev.k8s-test.orange-sonatel.com',
+  // SERVER_API_URL: 'https://selfcare-client-dsiselfcarebcorangeetmoi-isoprod.apps.paas.orange-sonatel.com',
   OM_URL: 'https://appom.orange-sonatel.com',
   SEDDO_SERVICE: 'selfcare-b2c-seddo',
   CONSO_SERVICE: 'selfcare-b2c-conso',
