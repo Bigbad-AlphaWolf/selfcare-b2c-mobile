9550943ef8e55d585c082c4f19c388c1af2a44e2
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-07-05 19:39:18 +0000

    SELFB2C-3560 Ajout du champ date de délivrance sur le formulaire annulation transaction OM, Initialisation de la variable listActes sur la page Assistance et Mise à jour de la méthode choix bénéficiaire pour les kirene
        -Ajouter le champ date de délivrance à la demande du metier
    
        -Initialiser les variables listActes et listFaq pour corriger les erreurs forEach of undefined sur la page Assistance
    
        -Corriger la redirection pour la page choix du montant sur le dashboard Kirene

--- src/app/assistance-hub/assistance-hub.page.ts
+++ src/app/assistance-hub/assistance-hub.page.ts
@@ -15,51 +15,47 @@ import { OrangeMoneyService } from '../services/orange-money-service/orange-mone
 @Component({
   selector: 'app-assistance-hub',
   templateUrl: './assistance-hub.page.html',
-  styleUrls: ['./assistance-hub.page.scss'],
+  styleUrls: ['./assistance-hub.page.scss']
 })
 export class AssistanceHubPage implements OnInit {
   slideOpts = {
     speed: 400,
     slidesPerView: 1.14,
     slideShadows: true,
-    loop: true,
+    loop: true
   };
   fastActions = [
     {
       act: 'DEPLAFONNEMENT',
       description: 'Déplafonner mon compte Orange Money',
-      image:
-        '/assets/images/04-boutons-01-illustrations-03-payer-ma-facture.svg',
+      image: '/assets/images/04-boutons-01-illustrations-03-payer-ma-facture.svg'
     },
     {
       act: 'TRANSACTION_ERROR',
       description: 'Déclarer une erreur de transaction',
-      image:
-        '/assets/images/04-boutons-01-illustrations-03-payer-ma-facture.svg',
+      image: '/assets/images/04-boutons-01-illustrations-03-payer-ma-facture.svg'
     },
     {
       act: 'FIBRE_OPTIC',
       description: 'Vérifier mon éligibilité à la fibre optique',
-      image:
-        '/assets/images/04-boutons-01-illustrations-04-paiement-marchand.svg',
-    },
+      image: '/assets/images/04-boutons-01-illustrations-04-paiement-marchand.svg'
+    }
   ];
   moreActions = [
     {
       act: 'IBOU_CONTACT',
       description: 'Contacter votre assistant Ibou',
-      image:
-        '/assets/images/04-boutons-01-illustrations-21-ibou-assistance.png',
+      image: '/assets/images/04-boutons-01-illustrations-21-ibou-assistance.png'
     },
     {
       act: 'AGENCE_LOCATOR',
       description: 'Trouver l’agence la plus proche',
-      image: '/assets/images/04-boutons-01-illustrations-22-store-locator.svg',
-    },
+      image: '/assets/images/04-boutons-01-illustrations-22-store-locator.svg'
+    }
   ];
-  listBesoinAides: OffreService[];
-  listFaqs?: OffreService[];
-  listActes?: OffreService[];
+  listBesoinAides: OffreService[] = [];
+  listFaqs?: OffreService[] = [];
+  listActes?: OffreService[] = [];
   loadingHelpItems: boolean;
   displaySearchIcon: boolean = true;
   @ViewChild('searchInput') searchRef;
@@ -91,16 +87,15 @@ export class AssistanceHubPage implements OnInit {
 
   filterOMActesFollowingOMStatus(user: OMCustomerStatusModel, actes: OffreService[]) {
     let response = actes;
-    if(user.operation === 'DEPLAFONNEMENT' || user.operation === 'FULL') {
+    if (user.operation === 'DEPLAFONNEMENT' || user.operation === 'FULL') {
       response = actes.filter((item: OffreService) => {
-        return item.code !== 'OUVERTURE_OM_ACCOUNT'
-      })
-    } else if(user.operation === 'OUVERTURE_COMPTE' ) {
+        return item.code !== 'OUVERTURE_OM_ACCOUNT';
+      });
+    } else if (user.operation === 'OUVERTURE_COMPTE') {
       response = actes.filter((item: OffreService) => {
-        return item.code !== 'DEPLAFONNEMENT'
-      })
+        return item.code !== 'DEPLAFONNEMENT';
+      });
     }
-    console.log('resp', response);
 
     return response;
   }
@@ -108,25 +103,19 @@ export class AssistanceHubPage implements OnInit {
   fetchAllHelpItems() {
     this.loadingHelpItems = true;
     this.operationService.getServicesByFormule(null, true).subscribe(
-      async (res) => {
+      async res => {
         this.userOMStatus = await this.checkStatus();
-        console.log('statusii', status);
-
         this.listBesoinAides = res;
         this.loadingHelpItems = false;
-        this.followAnalyticsService.registerEventFollow(
-          'Assistance_hub_affichage_success',
-          'event'
-        );
+        this.followAnalyticsService.registerEventFollow('Assistance_hub_affichage_success', 'event');
         this.splitHelpItemsByType();
       },
-      (err) => {
+      err => {
         this.loadingHelpItems = false;
-        this.followAnalyticsService.registerEventFollow(
-          'Assistance_hub_affichage_error',
-          'error',
-          { msisdn: this.current_user_msisdn, error: err.status }
-        );
+        this.followAnalyticsService.registerEventFollow('Assistance_hub_affichage_error', 'error', {
+          msisdn: this.current_user_msisdn,
+          error: err.status
+        });
       }
     );
   }
@@ -135,9 +124,7 @@ export class AssistanceHubPage implements OnInit {
     this.listActes = this.listBesoinAides.filter((item: OffreService) => {
       return item.typeService === BesoinAideType.ACTE;
     });
-    this.listActes = this.filterOMActesFollowingOMStatus(this.userOMStatus,this.listActes);
-    console.log('listActes', this.listActes);
-
+    this.listActes = this.filterOMActesFollowingOMStatus(this.userOMStatus, this.listActes);
     this.listFaqs = this.listBesoinAides.filter((item: OffreService) => {
       return item.typeService === BesoinAideType.FAQ;
     });
@@ -145,24 +132,16 @@ export class AssistanceHubPage implements OnInit {
 
   goAllActionsHub() {
     this.router.navigate(['/assistance-hub/actions'], {
-      state: { listActes: this.listActes },
+      state: { listActes: this.listActes }
     });
-    this.followAnalyticsService.registerEventFollow(
-      'Assistance_hub_voir_toutes_actions_clic',
-      'event',
-      'clicked'
-    );
+    this.followAnalyticsService.registerEventFollow('Assistance_hub_voir_toutes_actions_clic', 'event', 'clicked');
   }
 
   goAllQuestionsHub() {
     this.router.navigate(['/assistance-hub/questions'], {
-      state: { listFaqs: this.listFaqs },
+      state: { listFaqs: this.listFaqs }
     });
-    this.followAnalyticsService.registerEventFollow(
-      'Assistance_hub_voir_tous_faq_clic',
-      'event',
-      'clicked'
-    );
+    this.followAnalyticsService.registerEventFollow('Assistance_hub_voir_tous_faq_clic', 'event', 'clicked');
   }
 
   goBack() {
@@ -171,11 +150,7 @@ export class AssistanceHubPage implements OnInit {
 
   goFindToAgenceWebSite() {
     this.inAppBrowser.create(FIND_AGENCE_EXTERNAL_URL, '_self');
-    this.followAnalyticsService.registerEventFollow(
-      'Assistance_hub_Trouver_agence_orange_clic',
-      'event',
-      'clicked'
-    );
+    this.followAnalyticsService.registerEventFollow('Assistance_hub_Trouver_agence_orange_clic', 'event', 'clicked');
   }
 
   goFastAction(action) {
@@ -190,11 +165,7 @@ export class AssistanceHubPage implements OnInit {
   }
 
   async goIbouContactPage() {
-    this.followAnalyticsService.registerEventFollow(
-      'Assistance_Hub_Ibou_card_clic',
-      'event',
-      'clicked'
-    );
+    this.followAnalyticsService.registerEventFollow('Assistance_Hub_Ibou_card_clic', 'event', 'clicked');
     this.router.navigate(['/contact-ibou-hub']);
   }
 
@@ -203,13 +174,9 @@ export class AssistanceHubPage implements OnInit {
     this.displaySearchIcon = true;
     if (inputvalue) {
       this.navController.navigateForward(['/assistance-hub/search'], {
-        state: { listBesoinAides: this.listBesoinAides, search: inputvalue },
+        state: { listBesoinAides: this.listBesoinAides, search: inputvalue }
       });
-      this.followAnalyticsService.registerEventFollow(
-        'Assistance_hub_recherche',
-        'event',
-        { 'keyword': inputvalue }
-      );
+      this.followAnalyticsService.registerEventFollow('Assistance_hub_recherche', 'event', { keyword: inputvalue });
       this.displaySearchIcon = false;
     }
   }
--- src/app/models/cancel-om-transaction-payload.model.ts
+++ src/app/models/cancel-om-transaction-payload.model.ts
@@ -1,13 +1,14 @@
-export interface CancelOmTransactionPayloadModel{
-    civility? : string,
-    firstName? : string,
-    lastName? : string,
-    email? : string,
-    identityType? : string,
-    identityNumber? : any,
-    numero? : string,
-    msisdnBeneficiaire? : string
-    referenceTransaction? : string
-    dateTransaction? : string
-    montantTransfert? : string
+export interface CancelOmTransactionPayloadModel {
+  civility?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  identityType?: string;
+  identityNumber?: any;
+  numero?: string;
+  msisdnBeneficiaire?: string;
+  referenceTransaction?: string;
+  dateDelivrance?: string;
+  dateTransaction?: string;
+  montantTransfert?: string;
 }
