25355f6b8f75ae8c74e3867cf8d1a1ad9d3aebf1
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2021-08-18 16:31:50 +0000

    WIP

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,29 +1,30 @@
-import { NgModule } from '@angular/core';
-import { ItemOfferPlanComponent } from './item-offer-plan/item-offer-plan.component';
-import { ItemPassIllimixComponent } from './item-pass-illimix/item-pass-illimix.component';
-import { ItemPassInternetComponent } from './item-pass-internet/item-pass-internet.component';
-import { ItemRechargeCreditComponent } from './item-recharge-credit/item-recharge-credit.component';
-import { ItemRattachedNumberComponent } from './item-rattached-number/item-rattached-number.component';
-import { RattachNumberModalComponent } from '../pages/rattached-phones-number/components/rattach-number-modal/rattach-number-modal.component';
-import { RattachNumberByIdCardComponent } from '../pages/rattached-phones-number/components/rattach-number-by-id-card/rattach-number-by-id-card.component';
-import { RattachNumberByClientCodeComponent } from '../pages/rattached-phones-number/components/rattach-number-by-client-code/rattach-number-by-client-code.component';
-import { DalalCardItemComponent } from 'src/shared/dalal-card-item/dalal-card-item.component';
-import { DashboardHeaderComponent } from 'src/shared/dashboard-header/dashboard-header.component';
-import { CardRapidoNameModalComponent } from './card-rapido-name-modal/card-rapido-name-modal.component';
-import { ItemIlliflexComponent } from 'src/shared/illiflex-item/item-illiflex/item-illiflex.component';
-import { IdentifiedNumbersListComponent } from '../pages/rattached-phones-number/components/identified-numbers-list/identified-numbers-list.component';
-import { ChooseRattachementTypeModalComponent } from '../pages/rattached-phones-number/components/choose-rattachement-type-modal/choose-rattachement-type-modal.component';
-import { ItemHistorikDetailsInfosComponent } from '../details-conso/components/item-historik-details-infos/item-historik-details-infos.component';
-import { IonicImageLoader } from 'ionic-image-loader';
-import { CommonModule } from '@angular/common';
-import { PipesModule } from '../pipes/pipes.module';
-import { MaterialComponentsModule } from '../material-components/material-components.module';
-import { IonicModule } from '@ionic/angular';
-import { ItemTransfertOmComponent } from './item-transfert-om/item-transfert-om.component';
-import { SlideBanniereDashboardComponent } from './slide-banniere-dashboard/slide-banniere-dashboard.component';
-import { TakePictureComponent } from './take-picture/take-picture.component';
-import { HistorikTransactionByTypeModalComponent } from './historik-transaction-by-type-modal/historik-transaction-by-type-modal.component';
-import { ItemOmUserStatusCardInfosComponent } from './item-om-user-status-card-infos/item-om-user-status-card-infos.component';
+import {NgModule} from '@angular/core';
+import {ItemOfferPlanComponent} from './item-offer-plan/item-offer-plan.component';
+import {ItemPassIllimixComponent} from './item-pass-illimix/item-pass-illimix.component';
+import {ItemPassInternetComponent} from './item-pass-internet/item-pass-internet.component';
+import {ItemRechargeCreditComponent} from './item-recharge-credit/item-recharge-credit.component';
+import {ItemRattachedNumberComponent} from './item-rattached-number/item-rattached-number.component';
+import {RattachNumberModalComponent} from '../pages/rattached-phones-number/components/rattach-number-modal/rattach-number-modal.component';
+import {RattachNumberByIdCardComponent} from '../pages/rattached-phones-number/components/rattach-number-by-id-card/rattach-number-by-id-card.component';
+import {RattachNumberByClientCodeComponent} from '../pages/rattached-phones-number/components/rattach-number-by-client-code/rattach-number-by-client-code.component';
+import {DalalCardItemComponent} from 'src/shared/dalal-card-item/dalal-card-item.component';
+import {DashboardHeaderComponent} from 'src/shared/dashboard-header/dashboard-header.component';
+import {CardRapidoNameModalComponent} from './card-rapido-name-modal/card-rapido-name-modal.component';
+import {ItemIlliflexComponent} from 'src/shared/illiflex-item/item-illiflex/item-illiflex.component';
+import {IdentifiedNumbersListComponent} from '../pages/rattached-phones-number/components/identified-numbers-list/identified-numbers-list.component';
+import {ChooseRattachementTypeModalComponent} from '../pages/rattached-phones-number/components/choose-rattachement-type-modal/choose-rattachement-type-modal.component';
+import {ItemHistorikDetailsInfosComponent} from '../details-conso/components/item-historik-details-infos/item-historik-details-infos.component';
+import {IonicImageLoader} from 'ionic-image-loader';
+import {CommonModule} from '@angular/common';
+import {PipesModule} from '../pipes/pipes.module';
+import {MaterialComponentsModule} from '../material-components/material-components.module';
+import {IonicModule} from '@ionic/angular';
+import {ItemTransfertOmComponent} from './item-transfert-om/item-transfert-om.component';
+import {SlideBanniereDashboardComponent} from './slide-banniere-dashboard/slide-banniere-dashboard.component';
+import {TakePictureComponent} from './take-picture/take-picture.component';
+import {HistorikTransactionByTypeModalComponent} from './historik-transaction-by-type-modal/historik-transaction-by-type-modal.component';
+import {ItemOmUserStatusCardInfosComponent} from './item-om-user-status-card-infos/item-om-user-status-card-infos.component';
+import {ListPreviewStoriesComponent} from './list-preview-stories/list-preview-stories.component';
 
 @NgModule({
   declarations: [
@@ -46,15 +47,10 @@ import { ItemOmUserStatusCardInfosComponent } from './item-om-user-status-card-i
     HistorikTransactionByTypeModalComponent,
     TakePictureComponent,
     ItemTransfertOmComponent,
-    ItemOmUserStatusCardInfosComponent
+    ItemOmUserStatusCardInfosComponent,
+    ListPreviewStoriesComponent
   ],
-  imports: [
-    CommonModule,
-    PipesModule,
-    MaterialComponentsModule,
-    IonicModule,
-    IonicImageLoader,
-  ],
+  imports: [CommonModule, PipesModule, MaterialComponentsModule, IonicModule, IonicImageLoader],
   exports: [
     ItemOfferPlanComponent,
     ItemPassIllimixComponent,
@@ -76,7 +72,8 @@ import { ItemOmUserStatusCardInfosComponent } from './item-om-user-status-card-i
     TakePictureComponent,
     ItemTransfertOmComponent,
     ItemOmUserStatusCardInfosComponent,
+    ListPreviewStoriesComponent
   ],
-  entryComponents: [HistorikTransactionByTypeModalComponent],
+  entryComponents: [HistorikTransactionByTypeModalComponent]
 })
 export class ComponentsModule {}
---
+++ src/app/components/list-preview-stories/list-preview-stories.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-preview-stories',
+  templateUrl: './list-preview-stories.component.html',
+  styleUrls: ['./list-preview-stories.component.scss'],
+})
+export class ListPreviewStoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
