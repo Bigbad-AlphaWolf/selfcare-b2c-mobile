4fd3fff036b65e8e1338e7d8c1b0c9f72f9e7958
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-02-14 09:34:04 +0000

    Ajout config pour affichage notif au premier plan

--- src/app/services/batch-analytics/batch-analytics.service.ts
+++ src/app/services/batch-analytics/batch-analytics.service.ts
@@ -14,6 +14,7 @@ export class BatchAnalyticsService {
     batch.setConfig({androidAPIKey: BATCH_ANDROID_API_KEY, iOSAPIKey: BATCH_IOS_API_KEY});
     batch.start();
     if (isIOS) {
+      batch.push.setiOSShowForegroundNotifications(true);
       batch.push.registerForRemoteNotifications();
     }
   }
