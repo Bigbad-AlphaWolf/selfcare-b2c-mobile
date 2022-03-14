6efd6d173b295799ee7eb3a03c951a2cb1e9c53c
Papa Abdoulaye KEBE <ext_papaabdoulaye.kebe@orange-sonatel.com>
2022-03-14 15:30:12 +0000

    SELFB2C-4082 tagguer les events sur les stories

--- src/app/components/list-preview-stories/list-preview-stories.component.ts
+++ src/app/components/list-preview-stories/list-preview-stories.component.ts
@@ -4,14 +4,16 @@ import { ImageAttribute } from 'ionic-image-loader';
 import {of} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import {Story} from 'src/app/models/story-oem.model';
+import { DashboardService } from 'src/app/services/dashboard-service/dashboard.service';
+import { FollowAnalyticsService } from 'src/app/services/follow-analytics/follow-analytics.service';
 import {StoriesService} from 'src/app/services/stories-service/stories.service';
 import { VisualizeStoriesByCategoriesComponent } from '../visualize-stories-by-categories/visualize-stories-by-categories.component';
 import {VisualizeStoriesComponent} from '../visualize-stories/visualize-stories.component';
 
 @Component({
-  selector: 'app-list-preview-stories',
-  templateUrl: './list-preview-stories.component.html',
-  styleUrls: ['./list-preview-stories.component.scss']
+  selector: "app-list-preview-stories",
+  templateUrl: "./list-preview-stories.component.html",
+  styleUrls: ["./list-preview-stories.component.scss"],
 })
 export class ListPreviewStoriesComponent implements OnInit {
   @Input()
@@ -21,40 +23,55 @@ export class ListPreviewStoriesComponent implements OnInit {
       ordre?: number;
       code?: string;
       zoneAffichage?: string;
-			image?: string;
+      image?: string;
     };
     stories: Story[];
     readAll: boolean;
   }[];
   @Input() isLoadingStories: boolean;
   @Input() hasError: boolean;
-	@Input() areDataLoadedExternally: boolean;
-	isWeb: boolean;
-	imageAttributes: ImageAttribute[] = [{
-		element: 'class',
-		value: 'img-rounded'
-	}];
-  constructor(public modalController: ModalController, public animationCtrl: AnimationController, private storiesService: StoriesService, private platform: Platform) {
-		this.isWeb = platform.is('mobileweb')
-	}
+  @Input() areDataLoadedExternally: boolean;
+  isWeb: boolean;
+  imageAttributes: ImageAttribute[] = [
+    {
+      element: "class",
+      value: "img-rounded",
+    },
+  ];
+  currentMsisdn = this.dashboardService.getCurrentPhoneNumber();
+
+  constructor(
+    public modalController: ModalController,
+    public animationCtrl: AnimationController,
+    private storiesService: StoriesService,
+    private platform: Platform,
+    private followAnalyticsService: FollowAnalyticsService,
+    private dashboardService: DashboardService
+  ) {
+    this.isWeb = platform.is("mobileweb");
+  }
 
   ngOnInit() {
-		if(!this.areDataLoadedExternally) this.fetchUserStories();
+    if (!this.areDataLoadedExternally) this.fetchUserStories();
   }
 
   fetchUserStories() {
     this.isLoadingStories = true;
-		this.storiesByCategory = [];
+    this.storiesByCategory = [];
     this.hasError = false;
     this.storiesService
       .getCurrentStories()
       .pipe(
         tap((res: any) => {
           this.isLoadingStories = false;
-          console.log('groupeStoriesByCategory', this.storiesService.groupeStoriesByCategory(res));
-          this.storiesByCategory = this.storiesService.groupeStoriesByCategory(res);
+          console.log(
+            "groupeStoriesByCategory",
+            this.storiesService.groupeStoriesByCategory(res)
+          );
+          this.storiesByCategory =
+            this.storiesService.groupeStoriesByCategory(res);
         }),
-        catchError(err => {
+        catchError((err) => {
           this.isLoadingStories = false;
           this.hasError = true;
           return of(err);
@@ -73,42 +90,30 @@ export class ListPreviewStoriesComponent implements OnInit {
       };
       stories: Story[];
       readAll: boolean;
-    },
-    index: number
+    }
   ) {
-		let modal;
-		//if(item?.readAll) {
-		//	modal = await this.modalController.create({
-		//		component: VisualizeStoriesComponent,
-		//		backdropDismiss: true,
-		//		swipeToClose: true,
-		//		mode: 'ios',
-		//		presentingElement: await this.modalController.getTop(),
-		//		componentProps: {
-		//			index,
-		//			storyByCategory: item,
-		//			isVisibleForHigherView: true
-		//		}
-		//	});
-		//} else {
-			modal = await this.modalController.create({
-				component: VisualizeStoriesByCategoriesComponent,
-				backdropDismiss: true,
-				swipeToClose: true,
-				mode: 'ios',
-				presentingElement: await this.modalController.getTop(),
-				componentProps: {
-					allStories: this.storiesByCategory,
-					currentItem: item
-				}
-			});
-		//}
-    modal.onDidDismiss().then((res: {data: any}) => {
-			if(res?.data) {
-				this.updateInternalStoriesListe(res?.data?.index, res?.data?.storyByCategory)
-			} else {
-				this.fetchUserStories();
-			}
+    this.followAnalyticsService.registerEventFollow('story_clic', 'event', {msisdn: this.currentMsisdn})
+    const modal = await this.modalController.create({
+      component: VisualizeStoriesByCategoriesComponent,
+      backdropDismiss: true,
+      swipeToClose: true,
+      mode: "ios",
+      presentingElement: await this.modalController.getTop(),
+      componentProps: {
+        allStories: this.storiesByCategory,
+        currentItem: item,
+      },
+    });
+    modal.onDidDismiss().then((res) => {
+      res?.role ? this.followAnalyticsService.registerEventFollow('story_close', 'event', {msisdn: this.currentMsisdn}) : '';
+      if (res?.data) {
+        this.updateInternalStoriesListe(
+          res?.data?.index,
+          res?.data?.storyByCategory
+        );
+      } else {
+        this.fetchUserStories();
+      }
     });
     return await modal.present();
   }
@@ -127,7 +132,9 @@ export class ListPreviewStoriesComponent implements OnInit {
     }
   ) {
     this.storiesByCategory[index] = item;
-    this.storiesByCategory[index].readAll = !this.storiesByCategory[index].stories.filter(elt => {
+    this.storiesByCategory[index].readAll = !this.storiesByCategory[
+      index
+    ].stories.filter((elt) => {
       return elt.read === false;
     }).length;
   }
