3f6d7e843dd141af03336b348f13ae3dd305ec8d
Papa Abdoulaye KEBE <ext_papaabdoulaye.kebe@orange-sonatel.com>
2022-04-03 09:19:43 +0000

    SELFB2C-4077 test android integration

--- DimeloPlugin/src/android/DimeloCordovaPlugin.java
+++ DimeloPlugin/src/android/DimeloCordovaPlugin.java
@@ -14,6 +14,9 @@ import com.dimelo.dimelosdk.main.Chat;
 import com.dimelo.dimelosdk.main.Dimelo;
 import android.content.Context;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DimeloCordovaPlugin extends CordovaPlugin {
 
     @Override
@@ -33,10 +36,23 @@ public class DimeloCordovaPlugin extends CordovaPlugin {
     }
 
     public void initDimelo() {
-        Context context = this.cordova.getActivity().getApplicationContext(); 
+        Context context = this.cordova.getActivity().getApplicationContext();
         Dimelo dimelo = Dimelo.setup(context);
-        dimelo.initWithApiSecret("b25dc90dcaed229e01ff8ffe", "sonatel", null);
+        dimelo.initWithApiSecret("bb1e6f640bebc7e05863ff1117d44bc0def05726c9d6a3d503dddfcd50cf16d1", "sonatel", null);
+        dimelo.setUserName("John Doe");
+
+
+
+        JSONObject authInfo = new JSONObject();
+        try {
+            authInfo.put("CustomerId", "0123456789");
+            authInfo.put("Dimelo", "Rocks!");
+        } catch (JSONException e) {}
+
+        dimelo.setAuthenticationInfo(authInfo);
+
         Dimelo.getInstance().openRcActivity(this.cordova.getActivity());
+
         System.out.println(dimelo);
     }
 
