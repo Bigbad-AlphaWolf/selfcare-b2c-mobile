5b25cc2efc06623f05f349583cb14d2dbd26735f
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-06-30 15:30:58 +0000

    SELFB2C-4314 Cached img from stories with ionic-img-loader package

---
+++ src/shared/pipes/display-file-manager-image-for-image-loader.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DisplayFileManagerImageForImageLoaderPipe } from './display-file-manager-image-for-image-loader.pipe';
+
+describe('DisplayFileManagerImageForImageLoaderPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DisplayFileManagerImageForImageLoaderPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
