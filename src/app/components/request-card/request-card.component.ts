3cf4b4506937f48499ccf30aba94e64acca9c80e
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-11-30 11:38:03 +0000

    SELFB2C-4637  Update model Request by adding redirectTo attribute and add logic to redirect to Ibou chat

--- src/app/components/request-card/request-card.component.ts
+++ src/app/components/request-card/request-card.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { RequestOem } from 'src/app/models/request-oem.model';
-
+import { DimeloCordovaPlugin } from 'DimeloPlugin/ngx';
+import { RedirectionEnum, RequestOem } from 'src/app/models/request-oem.model';
+import * as SecureLS from 'secure-ls';
+import { InfosAbonneModel } from 'src/app/models/infos-abonne.model';
+import { DashboardService } from 'src/app/services/dashboard-service/dashboard.service';
+const ls = new SecureLS({ encodingType: 'aes' });
 @Component({
   selector: 'oem-request-card',
   templateUrl: './request-card.component.html',
@@ -8,9 +12,31 @@ import { RequestOem } from 'src/app/models/request-oem.model';
 })
 export class RequestCardComponent implements OnInit {
   @Input('request') request: RequestOem;
-  @Input ('fullDescription') fullDescription : boolean = false;
-  constructor() { }
+  @Input('fullDescription') fullDescription: boolean = false;
+  constructor(private chatDimelo: DimeloCordovaPlugin, private dashboardServ: DashboardService) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
+  callIbou() {
+    const user: InfosAbonneModel = ls.get('userInfos');
+    const username = `${user?.givenName} ${user?.familyName}`;
+    const msisdn = this.dashboardServ.getMainPhoneNumber();
+    this.chatDimelo
+      .openChat(username, msisdn)
+      .then(res => {
+        console.log('res', res);
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  }
+
+  procesRedirection() {
+    if (this.request.redirectTo === RedirectionEnum.IBOU) {
+      this.callIbou();
+    }
+    //else {
+    //	this.iab.open(this.request.redirectTo)
+    //}
+  }
 }
