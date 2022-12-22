3eb7e0c1aa8815de7cd9ff06a06f8fa61520d663
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2022-12-22 08:39:24 +0000

    Update implementation redirectTo Ibou on request-card

--- src/app/components/request-card/request-card.component.ts
+++ src/app/components/request-card/request-card.component.ts
@@ -4,6 +4,7 @@ import { RedirectionEnum, RequestOem } from 'src/app/models/request-oem.model';
 import * as SecureLS from 'secure-ls';
 import { InfosAbonneModel } from 'src/app/models/infos-abonne.model';
 import { DashboardService } from 'src/app/services/dashboard-service/dashboard.service';
+import { AssistanceService } from 'src/app/services/assistance.service';
 const ls = new SecureLS({ encodingType: 'aes' });
 @Component({
   selector: 'oem-request-card',
@@ -13,27 +14,13 @@ const ls = new SecureLS({ encodingType: 'aes' });
 export class RequestCardComponent implements OnInit {
   @Input('request') request: RequestOem;
   @Input('fullDescription') fullDescription: boolean = false;
-  constructor(private chatDimelo: DimeloCordovaPlugin, private dashboardServ: DashboardService) {}
+  constructor(private assistantServ: AssistanceService) {}
 
   ngOnInit() {}
 
-  callIbou() {
-    const user: InfosAbonneModel = ls.get('userInfos');
-    const username = `${user?.givenName} ${user?.familyName}`;
-    const msisdn = this.dashboardServ.getMainPhoneNumber();
-    this.chatDimelo
-      .openChat(username, msisdn)
-      .then(res => {
-        console.log('res', res);
-      })
-      .catch(err => {
-        console.log('err', err);
-      });
-  }
-
   procesRedirection() {
     if (this.request.redirectTo === RedirectionEnum.IBOU) {
-      this.callIbou();
+      this.assistantServ.openIbouDimeloChat();
     }
     //else {
     //	this.iab.open(this.request.redirectTo)
